a873bc587ed22a3ab1ae8383089908c316160cf7
vrfxlvmhemakbgrxkq9406DanielleThompson <olesya-z59690@rambler.ru>
2025-12-04 19:45:55 +0800

    add mock-utils-global.py, index.ts, email.rst

---
+++ email.rst
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\email.rst
+// Generated at: 2025-10-13T12:41:51.216Z
---
+++ frontend/web/src/sms/simple-fixture-ape/mock-utils-global.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\sms\simple-fixture-ape\mock-utils-global.py
+// Generated at: 2025-10-13T12:41:51.241Z
